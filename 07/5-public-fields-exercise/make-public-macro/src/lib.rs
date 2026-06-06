d1fd950f133f67f0b19492c950b38fc914ff789f
Hans-J. Schmid <knock@myopendoor.de>
2026-06-06 10:06:21 +0200

    reorganize project

---
+++ 07/5-public-fields-exercise/make-public-macro/src/lib.rs
@@ -0,0 +1,40 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{__private::ToTokens, Data::Struct, Fields::Named};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed, Visibility};
+
+#[proc_macro_attribute]
+pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
+  let mut derive_input = parse_macro_input!(item as DeriveInput);
+
+  let fields = if let Struct(DataStruct {
+    fields: Named(FieldsNamed { ref named, .. }),
+    ..
+  }) = derive_input.data
+  {
+    named
+  } else {
+    unimplemented!("only works for structs with named fields")
+  };
+
+  let builder_fields = fields.iter().map(|field| {
+    let name = &field.ident;
+    let ty = &field.ty;
+    quote! { pub #name: #ty }
+  });
+
+  let builder_fields = quote!(
+      {
+          #(#builder_fields,)*
+      }
+  );
+
+  derive_input.data = Data::Struct(DataStruct {
+    struct_token: Default::default(),
+    fields: Fields::Named(syn::parse2(builder_fields).unwrap()),
+    semi_token: None,
+  });
+  derive_input.vis = Visibility::Public(Default::default());
+
+  derive_input.to_token_stream().into()
+}
---
+++ 08/05-error-for-defaults/builder-code/src/lib.rs
@@ -0,0 +1,63 @@
+mod fields;
+
+use crate::fields::{builder_field_definitions, builder_init_values};
+use crate::fields::{builder_methods, optional_default_asserts, original_struct_setters};
+use quote::{format_ident, quote};
+use syn::{__private::TokenStream2, parse2};
+use syn::{Data::Struct, DataStruct, DeriveInput, Fields::Named, FieldsNamed};
+
+const DEFAULTS_ATTRIBUTE_NAME: &str = "builder_defaults";
+
+pub fn create_builder(item: TokenStream2) -> TokenStream2 {
+  let derive_input: DeriveInput = parse2(item).unwrap();
+  let name = derive_input.ident;
+  let builder = format_ident!("{}Builder", name);
+  let is_using_defaults: bool = derive_input
+    .attrs
+    .iter()
+    .any(|attribute| attribute.path().is_ident(DEFAULTS_ATTRIBUTE_NAME));
+
+  let fields = match derive_input.data {
+    Struct(DataStruct {
+      fields: Named(FieldsNamed { ref named, .. }),
+      ..
+    }) => named,
+    _ => unimplemented!("only implemented for structs"),
+  };
+  let builder_fields = builder_field_definitions(fields);
+  let builder_inits = builder_init_values(fields);
+  let builder_methods = builder_methods(fields);
+  let set_fields = original_struct_setters(fields, is_using_defaults);
+
+  let default_assertions = if is_using_defaults {
+    optional_default_asserts(fields)
+  } else {
+    vec![]
+  };
+
+  quote! {
+      struct #builder {
+          #(#builder_fields,)*
+      }
+
+      impl #builder {
+          #(#builder_methods)*
+
+          pub fn build(self) -> #name {
+              #name {
+                  #(#set_fields,)*
+              }
+          }
+      }
+
+      impl #name {
+          pub fn builder() -> #builder {
+              #builder {
+                  #(#builder_inits,)*
+              }
+          }
+      }
+
+      #(#default_assertions)*
+  }
+}
